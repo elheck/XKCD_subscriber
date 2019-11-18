7a7d26d222332ffc02d2728dfa2dfc55b85b914a
Aron <aron.heck@gmx.de>
2019-11-18 18:03:20 +0100

    Update sendMail.go

--- sendMail.go
+++ sendMail.go
@@ -8,13 +8,13 @@ import (
 )
 
 //SendMailToAddress sends mail
-func SendMailToAddress(address, path, title, user, pw string) {
+func SendMailToAddress(address, path, title, user, pw, smtpWithPort, smtpAddress string) {
 	mail := email.NewEmail()
 	mail.From = "XKCD Subscriber"
 	mail.To = []string{address}
 	mail.Subject = "Todays XKCD comic: " + title
 	mail.AttachFile(path)
 	mail.Text = []byte("Todays comic is attached to this mail")
-	mail.Send("mail.gmx.net:587", smtp.PlainAuth("", user, pw, "mail.gmx.net"))
+	mail.Send(smtpWithPort, smtp.PlainAuth("", user, pw, smtpAddress))
 	log.Println("Email sent to", address)
 }
